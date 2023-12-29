df13ecdf4c863353d5f1d5cd22d650048190364a
Brett <brett_neighbors@yahoo.com>
2023-12-28 23:31:11 -0700

    moved each fx to match solution of lesson

--- index.js
+++ index.js
@@ -1,39 +1,106 @@
-// Create a submit handler
-
-const submitHandler = (event) => {
+function validateExists(value) {
+    return value && value.trim();
+  }
+  
+  function validateNumber(value) {
+    return !isNaN(value);
+  }
+  
+  function validateRange(value, min, max) {
+    return value >= min && value <= max;
+  }
+  
+  function validateDate(value) {
+    const date = moment(value);
+    return date.isValid();
+  }
+  
+  function validateForm(formData) {
+    const errors = {};
+  
+    // check if name was entered
+    if (!validateExists(formData.get("name"))) {
+      errors.name = "Please enter a name";
+    }
+  
+    // check if rating was entered
+    if (!validateExists(formData.get("rating"))) {
+      errors.rating = "Please enter a rating";
+    } else {
+      //check if the raying is a number
+      if (!validateNumber(formData.get("rating"))) {
+        errors.rating = "Rating must be a number";
+      } else {
+        // since it is a number, convert it
+        const rating = Number.parseFloat(formData.get("rating"));
+        //check that the rating is between 1 and 5 inclusive
+        if (!validateRange(rating, 1, 5)) {
+          errors.rating = "Rating must be between 1 and 5 inclusive";
+        }
+      }
+    }
+  
+    // check if description was entered
+    if (!validateExists(formData.get("description"))) {
+      errors.description = "Please enter short description";
+    }
+  
+    // check if established date was entered
+    if (!validateExists(formData.get("established"))) {
+      errors.established = "Please enter date";
+    } else {
+      // check if the value entered was a correct date
+      if (!validateDate(formData.get("established"))) {
+        errors.established =
+          "The date is not correctly formatted. (e.g. July 4, 1776)";
+      }
+    }
+  
+    // check if area was entered
+    if (!validateExists(formData.get("area"))) {
+      errors.area = "Please enter the area of the park";
+    }
+  
+    // check if location date was entered
+    if (!validateExists(formData.get("location"))) {
+      errors.location = "Please enter the location of the park";
+    }
+  
+    return errors;
+  }
+  
+  const submitHandler = (event) => {
     event.preventDefault();
-
+  
     const form = event.target;
     const formData = new FormData(form);
   
     const errors = validateForm(formData);
   
-    // Clear all previous errors
+    // clear all previous errors
     const errorElements = document.querySelectorAll(".error");
     for (let element of errorElements) {
       element.style.display = "none";
     }
   
-    // Display any new errors
+    // display any new errors
     Object.keys(errors).forEach((key) => {
-      // Find the specific error element
+      // find the specific error element
       const errorElement = document.querySelector(`#${key}-form .error`);
       errorElement.innerHTML = errors[key];
       errorElement.style.display = "block";
     });
-
-      // Add a park to the DOM 
-
-// If there are no errors
-if (!Object.keys(errors).length) {
-    // Create a new element
-    const parkSection = document.createElement("section");
   
-    // Add the park class
-    parkSection.classList.add("park-display");
+    // if there are no errors
+    if (!Object.keys(errors).length) {
+      //create a new element
+      const parkSection = document.createElement("section");
   
-    // Construct the HTML for this element
-    const content = `
+      // add the park class
+      parkSection.classList.add("park-display");
+  
+      // construct the HTML for this element
+      const content = `
       <h2>${formData.get("name")}</h2>
       <div class="location-display">${formData.get("location")}</div>
       <div class="description-display">${formData.get("description")}</div>
@@ -56,66 +123,21 @@ if (!Object.keys(errors).length) {
       </div>
       `;
   
-    // Set the innerHTML
-    parkSection.innerHTML = content;
+      // set the innerHTML
+      parkSection.innerHTML = content;
   
-    // Append to the main element
-    document.querySelector("main").appendChild(parkSection);
-  }
-}
-
-// Then add main() function that creates event listener for the form submission
-const main = () => {
-    // Get form element
+      //append to the main element
+      document.querySelector("main").appendChild(parkSection);
+    }
+  };
+  
+  const main = () => {
+    // get the form element
     const form = document.querySelector("#park-form");
-
-    // Attach the submit handler
+  
+    // attach the submit handler
     form.addEventListener("submit", submitHandler);
-}
-
-// Add event listener for DOMContenetLoaded that invokes main() function
-window.addEventListener("DOMContentLoaded", main);
-
-// Add validate function for required fields
-function validateExists(value) {
-    return value && value.trim();
-}
-
-// Add validateForm function
-function validateForm(formData) {
-    const errors = {};
+  };
   
-    // Check if name was entered
-    if (!validateExists(formData.get("name"))) {
-      errors.name = "Please enter a name";
-    }
-  
-    // Check if rating was entered
-    if (!validateExists(formData.get("rating"))) {
-      errors.rating = "Please enter a rating";
-    }
-  
-    // Check if description was entered
-    if (!validateExists(formData.get("description"))) {
-      errors.description = "Please enter short description";
-    }
-  
-    // Check if established date was entered
-    if (!validateExists(formData.get("established"))) {
-      errors.established = "Please enter date";
-    }
-  
-    // Check if area was entered
-    if (!validateExists(formData.get("area"))) {
-      errors.area = "Please enter the area of the park";
-    }
-  
-    // Check if location date was entered
-    if (!validateExists(formData.get("location"))) {
-      errors.location = "Please enter the location of the park";
-    }
-  
-    return errors;
-  }
-
+  window.addEventListener("DOMContentLoaded", main);
 
