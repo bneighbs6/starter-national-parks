6b5c38aca35be86b4dd5e6b3bbde5b4241beb154
Brett <brett_neighbors@yahoo.com>
2023-12-28 22:53:11 -0700

    submitHandler() and main() functions created

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Create a submit handler
 
+const submitHandler = (event) => {
+    // prevent default
+    event.preventDefault();
+    console.log("Your form was submitted");
+}
+
+// Then add main() function that creates event listener for the form submission
+const main = () => {
+    // Get form element
+    const form = document.querySelector("#park-form");
+
+    // Attach the submit handler
+    form.addEventListener("submit", submitHandler);
+}
+
+// Add event listener for DOMContenetLoaded that invokes main() function
+window.addEventListener("DOMContentLoaded", main);
