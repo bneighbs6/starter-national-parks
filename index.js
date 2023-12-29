e1cb27eb19ac35ae9f95ee70d618d0465b855707
Brett <brett_neighbors@yahoo.com>
2023-12-28 23:12:40 -0700

    Added validateForm() & validateExists() functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const submitHandler = (event) => {
     const formData = new FormData(event.target);
 
     // get park name value
-    const name = FormData.get("name");
+    const name = formData.get("name");
     console.log(name);
 }
 
@@ -23,3 +23,45 @@ const main = () => {
 
 // Add event listener for DOMContenetLoaded that invokes main() function
 window.addEventListener("DOMContentLoaded", main);
+
+// Add validate function for required fields
+function validateExists(value) {
+    return value && value.trim();
+}
+
+// Add validateForm function
+function validateForm(formData) {
+    const errors = {};
+  
+    // Check if name was entered
+    if (!validateExists(formData.get("name"))) {
+      errors.name = "Please enter a name";
+    }
+  
+    // Check if rating was entered
+    if (!validateExists(formData.get("rating"))) {
+      errors.rating = "Please enter a rating";
+    }
+  
+    // Check if description was entered
+    if (!validateExists(formData.get("description"))) {
+      errors.description = "Please enter short description";
+    }
+  
+    // Check if established date was entered
+    if (!validateExists(formData.get("established"))) {
+      errors.established = "Please enter date";
+    }
+  
+    // Check if area was entered
+    if (!validateExists(formData.get("area"))) {
+      errors.area = "Please enter the area of the park";
+    }
+  
+    // Check if location date was entered
+    if (!validateExists(formData.get("location"))) {
+      errors.location = "Please enter the location of the park";
+    }
+  
+    return errors;
+  }
